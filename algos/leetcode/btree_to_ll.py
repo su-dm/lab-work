ec33d536695110abe2b16e61aefb815be50f9e05
su-dm <sudm711@gmail.com>
2026-01-28 16:53:13 -0500

    leetcode

--- algos/leetcode/btree_to_ll.py
+++ algos/leetcode/btree_to_ll.py
@@ -53,3 +53,30 @@ class Solution:
             elif right:
                 stack.append(right)
         return root
+
+
+# beauty O(1) space
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        # Handle the null scenario
+        if not root:
+            return None
+
+        node = root
+        while node:
+
+            # If the node has a left child
+            if node.left:
+
+                # Find the rightmost node
+                rightmost = node.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+
+                # rewire the connections
+                rightmost.right = node.right
+                node.right = node.left
+                node.left = None
+
+            # move on to the right side of the tree
+            node = node.right
