be0bb77d3b72956bfd998619add38a232ad85b48
su-dm <sudm711@gmail.com>
2026-04-09 23:23:09 -0400

    leetcode

---
+++ algos/leetcode/max_rotate_func.py
@@ -0,0 +1,45 @@
+"""
+You are given an integer array nums of length n.
+
+Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+
+F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+Return the maximum value of F(0), F(1), ..., F(n-1).
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,6]
+Output: 26
+Explanation:
+F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
+"""
+
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        N = len(nums)
+        def F(k):
+            total = 0
+            for i in range(N):
+                total += nums[(i+k)%N] * i
+            return total
+        ans = float('-inf')
+        for k in range(N):
+            ans = max(ans, F(k))
+        return ans
+
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        N = len(nums)
+        total = sum(nums)
+        dp = [0] * N
+        dp[0] = sum(i*nums[i] for i in range(N))
+        for k in range(1, N):
+            dp[k] = dp[k-1] + total - N*nums[-k]
+        return max(dp)
