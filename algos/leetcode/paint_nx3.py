59e49b01f6645fb65943fee0599330cf3cb2dd69
su-dm <sudm711@gmail.com>
2026-04-10 17:49:44 -0400

    leetcode

---
+++ algos/leetcode/paint_nx3.py
@@ -0,0 +1,18 @@
+"""
+You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors: Red, Yellow, or Green while making sure that no two adjacent cells have the same color (i.e., no two cells that share vertical or horizontal sides have the same color).
+
+Given n the number of rows of the grid, return the number of ways you can paint this grid. As the answer may grow large, the answer must be computed modulo 109 + 7.
+
+Input: n = 1
+Output: 12
+Explanation: There are 12 possible way to paint the grid as shown.
+"""
+
+
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        MOD = 10**9 + 7
+        aba, abc = 6,6
+        for i in range(1, n):
+            aba, abc = (aba*3 + abc*2) % MOD, (aba*2 + abc*2) % MOD
+        return (aba + abc) % MOD
